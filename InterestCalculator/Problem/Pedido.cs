3f7dcfe5703082e04a3b99098ab540cd819d948b
Webert Lopes Cancado <webert.cancado@localiza.com>
2022-01-13 15:02:10 -0300

    Design pattern Strategy

---
+++ InterestCalculator/Problem/Pedido.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterestCalculator.Problem
+{
+    public class Pedido
+    {
+        public decimal Total { get; set; }
+        public string? Banco { get; set; }
+
+        public Pedido(decimal total, string banco)
+        {
+            this.Total = total;
+            this.Banco = banco;
+        }
+        public Decimal ObterPercentualJuros() 
+        {
+            return 10;
+        }       
+
+    }
+
+}
---
+++ InterestCalculator/Solucao/Santander.cs
@@ -0,0 +1,18 @@
+﻿using CalculadoraBancaria.Solution;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterestCalculator.Solution
+{
+    public class Santander : ICalculaJuros
+    {
+        public decimal ObterTaxaJuros(Pedido pedido)
+        {
+            var total = pedido.Taxa();
+            return total * 15 / 100;
+        }
+    }
+}
